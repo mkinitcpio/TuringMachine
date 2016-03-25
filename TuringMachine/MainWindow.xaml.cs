fbbc8bc17c1589d7e8044cb4785ac915b906c704
zzz3bra <z3vs@yandex.ru>
2016-03-25 21:56:03 +0300

    Create Turing Machine logic and interface interaction. Holy spirit inside!

--- TuringMachine/MainWindow.xaml.cs
+++ TuringMachine/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace TuringMachine
     /// </summary>
     public partial class MainWindow : Window
     {
+        TuringImplementation turing;
         public MainWindow()
         {
             InitializeComponent();
+            States.Text = "q0\r\nq1\r\nq2\r\nq3";
+            Alphabet.Text = "0\r\n1\r\nB";
+            StartState.Text = "q1";
+            StartTape.Text = "10111";
+            CodeBox.Text =
+@"// Прибавление единицы
+// к двоичному числу
+1q1->1q1R
+0q1->0q1R
+Bq1->Bq2L
+1q2->0q2L
+0q2->1q3L
+Bq2->1q0
+0q3->0q3L
+1q3->1q3L
+Bq3->Bq0R";
+
+        }
+
+        private void RunButton_Click(object sender, RoutedEventArgs e)
+        {
+            HashSet<string> states = new HashSet<string>();
+            string[] statesArray = States.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in statesArray)
+            {
+                states.Add(item);
+            }
+            HashSet<char> alphabet = new HashSet<char>();
+            string[] symbolsArray = Alphabet.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in symbolsArray)
+            {
+                alphabet.Add(item[0]);
+            }
+            turing = new TuringImplementation(states, StartState.Text, alphabet, StartTape.Text, CodeBox.Text);
+            StepButton.IsEnabled = true;
+        }
+
+        private void StepButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                turing.Step();
+                NextCommand.Text = turing.GetNextCommand();
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("That's all, folks!");
+                StepButton.IsEnabled = false;
+            }
+            Tape.Text = turing.Tape;
+            State.Text = turing.State;
+            Tape.Focus();
+            Tape.Select(turing.Position, 1);
+
         }
     }
 }
---
+++ TuringMachine/TuringImplementation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuringMachine
+{
+    class TuringImplementation
+    {
+        public string State { get; private set; }
+        public int Position { get; private set; }
+        public string Tape { get; private set; }
+        public string NextCommand { get; private set; }
+        private HashSet<string> States = new HashSet<string>();
+        private HashSet<char> Alphabet = new HashSet<char>();
+        private Dictionary<string, string> Commands = new Dictionary<string, string>();
+        private string InternalState;
+        public TuringImplementation(HashSet<string> states, string startState, HashSet<char> alphabet, string initialTape, string commands)
+        {
+            Position = 0;
+            States = states;
+            State = startState;
+            Alphabet = alphabet;
+            Tape = initialTape.Trim('0');
+            string[] coms = commands.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in coms)
+            {
+                if (item.StartsWith("//"))
+                {
+                    continue;
+                }
+                string[] temp = item.Split(new string[] { "->" }, StringSplitOptions.None);
+                Commands.Add(temp[0], temp[1]);
+            }
+            InternalState = Tape[Position] + startState;
+        }
+        public void Step()
+        {
+            string command = Commands[InternalState];
+            if (!Alphabet.Contains(command[0]))
+            {
+                throw new Exception("I don't know that symbol");
+            }
+            Tape = Tape.Substring(0, Position) + command[0] + Tape.Substring(Position + 1, Tape.Length - Position - 1);
+            command = command.Substring(1);
+
+            string newState = "";
+            foreach (var state in States)
+            {
+                if (command.StartsWith(state))
+                {
+                    newState = state;
+                    break;
+                }
+            }
+            if (string.IsNullOrEmpty(newState))
+            {
+                throw new Exception("Bad new state");
+            }
+            else
+            {
+                State = newState;
+            }
+            command = command.Remove(0, newState.Length);
+            if (!string.IsNullOrEmpty(command))
+            {
+                if (command.ToUpper().Equals("R"))
+                {
+                    Position++;
+                }
+                else if (command.ToUpper().Equals("L"))
+                {
+                    Position--;
+                }
+                else
+                {
+                    throw new Exception("Пойди туда не знаю куда");
+                }
+            }
+            if (Position == Tape.Length)
+            {
+                Tape += "B";
+            }
+            else if (Position < 0)
+            {
+                Tape = "B" + Tape;
+                Position++;
+            }
+            InternalState = Tape[Position] + State;
+            if (State == "q0")
+            {
+                throw new DivideByZeroException();
+            }
+        }
+        public string GetNextCommand()
+        {
+            return InternalState + "->" + Commands[InternalState];
+        }
+    }
+}
